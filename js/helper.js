e5c21e42145a4867d773e42f581c561a5841ba2f
oz <32149009+shallowbug@users.noreply.github.com>
2023-04-26 15:52:20 -0700

    visual changes

--- js/helper.js
+++ js/helper.js
@@ -1,3 +1,18 @@
+window.onscroll = function() {
+  let head = document.getElementById("head");
+  if (window.pageYOffset > 200){
+    head.classList.add("animate__slideInDown");
+    head.classList.add("sticky");
+  } else {
+    head.classList.remove("sticky");
+  }
+};
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 function moreShow(sec) {
 
   document.querySelector(sec + ' .moreButton').classList.add("hidden");
